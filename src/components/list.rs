061c0b857edad80abc36ead1b61fd7d5783a77cd
bioerrorlog <n.iijima.0x01@gmail.com>
2022-03-08 09:47:18 +0900

    mutable todo list

--- src/components/list.rs
+++ src/components/list.rs
@@ -1,30 +1,17 @@
-use yew::{function_component, html, Html};
+use yew::{function_component, html, Html, Properties};
 use crate::components::item::Item;
 use crate::components::types::Todo;
 
-#[function_component(List)]
-pub fn list() -> Html {
-    let todos = vec![
-        Todo {
-            id: 1,
-            title: "Learn Rust".to_string(),
-            completed: false,
-        },
-        Todo {
-            id: 2,
-            title: "Learn Yew".to_string(),
-            completed: true,
-        },
-        Todo {
-            id: 3,
-            title: "Learn Motoko".to_string(),
-            completed: false,
-        },
-    ];
+#[derive(Properties, PartialEq)]
+pub struct ListProps {
+    pub todo_items: Vec<Todo>,
+}
 
+#[function_component(List)]
+pub fn list(props: &ListProps) -> Html {
     html! {
         <ul class="list-group">
-            {todos.iter().map(|todo| html! {
+            {props.todo_items.iter().map(|todo| html! {
                 <Item title={todo.title.clone()} completed={todo.completed} />
             }).collect::<Html>()}
         </ul>
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,26 @@ use components::header::Header;
 use components::list::List;
 use components::form::Form;
 
+use crate::components::types::Todo;
+
 mod components;
 
 #[function_component(App)]
 fn app() -> Html {
+    let todo_items = use_state(|| Vec::<Todo>::new());
+    let next_id = use_state(|| 1);
+
     let on_add = {
+        let todo_items = todo_items.clone();
         Callback::from(move |title: String| {
-            log::info!("on_add: {:?}", title);
+            let mut current_todo_items = (*todo_items).clone();
+            current_todo_items.push(Todo {
+                id: *next_id,
+                title,
+                completed: false,
+            });
+            next_id.set(*next_id + 1);
+            todo_items.set(current_todo_items);
         })
     };
 
@@ -18,7 +31,7 @@ fn app() -> Html {
             <Header />
             <main class="container-fluid">
                 <Form {on_add}/>
-                <List />
+                <List todo_items={(*todo_items).clone() }/>
             </main>
         </>
     }
